ac12b90876f7618a4361df29e7076e19a5b12667
Douglas Christopher Wilson <doug@somethingdoug.com>
2015-01-08 17:59:52 -0500

    build: add source comments

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+/*!
+ * uid-safe
+ * Copyright(c) 2014 Jonathan Ong
+ * MIT Licensed
+ */
+
+
+/**
+ * Module dependencies.
+ * @private
+ */
 
 var Promise
 var pseudoRandomBytes = require('crypto').pseudoRandomBytes
 var escape = require('base64-url').escape
 
+/**
+ * Module exports.
+ */
+
 module.exports = uid
+module.exports.sync = uidSync
+
+/**
+ * Create a unique ID.
+ *
+ * @param {number} length
+ * @param {function} [callback]
+ * @return {Promise}
+ * @public
+ */
 
 function uid(length, callback) {
   if (callback) {
@@ -22,17 +47,41 @@ function uid(length, callback) {
   })
 }
 
-uid.sync = function uid_sync(length) {
-  return escapeBuffer(pseudoRandomBytes(length))
+/**
+ * Create a unique ID sync.
+ *
+ * @param {number} length
+ * @return {string}
+ * @public
+ */
+
+function uidSync(length) {
+  return toString(pseudoRandomBytes(length))
 }
 
-function escapeBuffer(buf) {
-  return escape(buf.toString('base64'))
-}
+/**
+ * Generate a unique ID string.
+ *
+ * @param {number} length
+ * @param {function} callback
+ * @private
+ */
 
 function generateUid(length, callback) {
   pseudoRandomBytes(length, function (err, buf) {
     if (err) return cb(err)
-    callback(null, escapeBuffer(buf))
+    callback(null, toString(buf))
   })
+}
+
+/**
+ * Change a Buffer into a string.
+ *
+ * @param {Buffer} buf
+ * @return {string}
+ * @private
+ */
+
+function toString(buf) {
+  return escape(buf.toString('base64'))
 }
