e7b00ad928dc84685418d3515eff97ab974c5d9f
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-08-10 20:02:35 -0400

    tests: use strict equality

--- test/test.js
+++ test/test.js
@@ -18,15 +18,15 @@ describe('uid()', function () {
 
     it('should return a uid of the correct length', function () {
       return uid(18).then(function (val) {
-        assert.equal(24, Buffer.byteLength(val))
+        assert.strictEqual(24, Buffer.byteLength(val))
       })
     })
 
     it('should not contain +, /, or =', function () {
       return uid(100000).then(function (val) {
-        assert(!~val.indexOf('+'))
-        assert(!~val.indexOf('/'))
-        assert(!~val.indexOf('='))
+        assert.strictEqual(val.indexOf('+'), -1)
+        assert.strictEqual(val.indexOf('/'), -1)
+        assert.strictEqual(val.indexOf('='), -1)
       })
     })
   })
@@ -55,7 +55,7 @@ describe('uid()', function () {
     it('should return a uid of the correct length', function (done) {
       uid(18, function (err, val) {
         if (err) return done(err)
-        assert.equal(24, Buffer.byteLength(val))
+        assert.strictEqual(Buffer.byteLength(val), 24)
         done()
       })
     })
@@ -63,9 +63,9 @@ describe('uid()', function () {
     it('should not contain +, /, or =', function (done) {
       uid(1000000, function (err, val) {
         if (err) return done(err)
-        assert(!~val.indexOf('+'))
-        assert(!~val.indexOf('/'))
-        assert(!~val.indexOf('='))
+        assert.strictEqual(val.indexOf('+'), -1)
+        assert.strictEqual(val.indexOf('/'), -1)
+        assert.strictEqual(val.indexOf('='), -1)
         done()
       })
     })
@@ -75,13 +75,13 @@ describe('uid()', function () {
 describe('uid.sync()', function () {
   it('should return a uid of the correct length', function () {
     var val = uid.sync(18)
-    assert.equal(24, Buffer.byteLength(val))
+    assert.strictEqual(Buffer.byteLength(val), 24)
   })
 
   it('should not contain +, /, or =', function () {
     var val = uid.sync(100000)
-    assert(!~val.indexOf('+'))
-    assert(!~val.indexOf('/'))
-    assert(!~val.indexOf('='))
+    assert.strictEqual(val.indexOf('+'), -1)
+    assert.strictEqual(val.indexOf('/'), -1)
+    assert.strictEqual(val.indexOf('='), -1)
   })
 })
